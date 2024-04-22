3d336e650fe0c9f97e2f8b9ad712224b31a503df
Julia Polyakova <razvlechenia.svoboda@yandex.ru>
2024-04-22 18:57:40 +0500

    add welcome page

--- src/app/children/authorization/pages/welcome/welcome.component.ts
+++ src/app/children/authorization/pages/welcome/welcome.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
-  selector: 'app-welcome',
-  templateUrl: './welcome.component.html',
-  styleUrl: './styles/welcome.component.css'
+    selector: 'app-welcome',
+    templateUrl: './welcome.component.html',
+    styleUrl: './styles/welcome.component.css',
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class WelcomeComponent {
 
+export class WelcomeComponent {
+    constructor(
+        private _router: Router,
+    ) {
+    }
 }
