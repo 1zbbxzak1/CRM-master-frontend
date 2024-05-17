69a4b17d4d9ebe5625283c227ba20c6b0a5b6c8d
Julia Polyakova <razvlechenia.svoboda@yandex.ru>
2024-05-17 21:45:54 +0500

    add a product page

---
+++ src/app/children/crm/pages/products/components/delete-product/delete-product.component.ts
@@ -0,0 +1,79 @@
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, DestroyRef, Inject, Input} from '@angular/core';
+import {Observer} from "rxjs";
+import {PolymorpheusContent} from "@tinkoff/ng-polymorpheus";
+import {TuiDialogContext, TuiDialogService} from "@taiga-ui/core";
+import {takeUntilDestroyed} from "@angular/core/rxjs-interop";
+import {TuiDialogFormService} from "@taiga-ui/kit";
+import {ProductsManagerService} from "../../../../../../data/services/products/products.manager.service";
+import {IProductsResponseModel} from "../../../../../../data/response-models/products/IProducts.response-model";
+import {ProductsComponent} from "../../products.component";
+
+@Component({
+    selector: 'app-delete-product',
+    templateUrl: './delete-product.component.html',
+    styleUrl: './styles/delete-product.component.css',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DeleteProductComponent {
+
+    @Input() observer?: Observer<never>;
+    @Input() id!: string;
+    protected products: IProductsResponseModel[] = [];
+
+
+    constructor(
+        @Inject(TuiDialogFormService)
+        private readonly _dialogForm: TuiDialogFormService,
+        @Inject(TuiDialogService)
+        private readonly _dialogs: TuiDialogService,
+        private readonly _destroyRef: DestroyRef,
+        private readonly _productsManagerService: ProductsManagerService,
+        private readonly _changeDetectorRef: ChangeDetectorRef,
+        private readonly _productsComponent: ProductsComponent,
+    ) {
+    }
+
+    public openDialogDelete(
+        deleteProduct: PolymorpheusContent<TuiDialogContext>,
+    ): void {
+        this._dialogs.open(
+            deleteProduct,
+            {
+                size: "s",
+            })
+            .pipe(
+                takeUntilDestroyed(this._destroyRef)
+            )
+            .subscribe({
+                complete: (): void => {
+                    this._dialogForm.markAsPristine();
+                },
+            });
+    }
+
+    fetchProducts(): void {
+        this._productsManagerService.getAllProducts().pipe(
+            takeUntilDestroyed(this._destroyRef)
+        ).subscribe((products: IProductsResponseModel[]): void => {
+            this.products = products;
+            console.log('Products received:', products);
+            this._changeDetectorRef.detectChanges();
+        });
+    }
+
+    protected deleteProduct(id: string): void {
+        this._productsManagerService.deleteProduct(id).pipe(
+            takeUntilDestroyed(this._destroyRef),
+        ).subscribe(
+            (): void => {
+                this._productsComponent.fetchProducts();
+                this.onCancel();
+            });
+    }
+
+    protected onCancel(): void {
+        if (this.observer) {
+            this.observer.complete();
+        }
+    }
+}
--- src/app/children/crm/pages/products/products.component.ts
+++ src/app/children/crm/pages/products/products.component.ts
@@ -1,4 +1,10 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, DestroyRef, OnInit, ViewChild} from '@angular/core';
+import {ProductsManagerService} from "../../../../data/services/products/products.manager.service";
+import {IProductsResponseModel} from "../../../../data/response-models/products/IProducts.response-model";
+import {takeUntilDestroyed} from "@angular/core/rxjs-interop";
+import {PolymorpheusContent} from "@tinkoff/ng-polymorpheus";
+import {TuiDialogContext} from "@taiga-ui/core";
+import {DeleteProductComponent} from "./components/delete-product/delete-product.component";
 
 @Component({
     selector: 'app-products',
@@ -6,6 +12,49 @@ import {ChangeDetectionStrategy, Component} from '@angular/core';
     styleUrls: ['../../styles/crm-styles.css', './styles/products.component.css'],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+    protected products: IProductsResponseModel[] = [];
+    protected search: string = '';
+    @ViewChild(DeleteProductComponent)
+    private readonly _deleteProductComponent!: DeleteProductComponent;
 
+    constructor(
+        private readonly _productsManagerService: ProductsManagerService,
+        private readonly _destroyRef: DestroyRef,
+        private readonly _changeDetectorRef: ChangeDetectorRef,
+    ) {
+    }
+
+    ngOnInit(): void {
+        this.fetchProducts();
+    }
+
+    fetchProducts(): void {
+        this._productsManagerService.getAllProducts().pipe(
+            takeUntilDestroyed(this._destroyRef)
+        ).subscribe((products: IProductsResponseModel[]): void => {
+            this.products = products;
+            console.log('Products received:', products);
+            this._changeDetectorRef.detectChanges();
+        });
+    }
+
+    protected searchProducts(): IProductsResponseModel[] {
+        return this.products.filter(product => product.name.toLowerCase().includes(this.search.toLowerCase()));
+    }
+
+    protected openDialogDelete(
+        deleteProduct: PolymorpheusContent<TuiDialogContext>,
+    ): void {
+        this._deleteProductComponent.openDialogDelete(deleteProduct);
+    }
+
+    protected toggleVisibility(id: string): void {
+        this._productsManagerService.toggleVisibility(id).pipe(
+            takeUntilDestroyed(this._destroyRef),
+        ).subscribe(
+            (): void => {
+                this.fetchProducts();
+            });
+    }
 }
